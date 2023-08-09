77f4ada70107a53c2a7d27629aa4232306e2e4e2
Ben <benjyc@proton.me>
2023-08-09 12:00:18 +0700

    ops

--- runtime.go
+++ runtime.go
@@ -1,24 +1,25 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"stack"
 )
 
 const (
-	OP_TRANSFER byte = 0x00
-	OP_PUSH     byte = 0x10
-	OP_EQUALVERIFY byte = 0x11
+	OP_TRANSFER     byte = 0x00
+	OP_PUSH         byte = 0x10
+	OP_EQUALVERIFY  byte = 0x11
 	OP_CHECKSIGCALL byte = 0x12
-	OP_KHASH       byte = 0x14
-	OP_SHA256     byte = 0x15
-	OP_NMREG      byte = 0x20
-	OP_NMLOAD     byte = 0x21
+	OP_KHASH        byte = 0x14
+	OP_SHA256       byte = 0x15
+	OP_NMREG        byte = 0x20
+	OP_NMLOAD       byte = 0x21
 )
 
 type State struct {
-	bal  map[string]int
+	bal   map[string]int
 	names map[string]string
 }
 
@@ -48,21 +49,133 @@ func (r *Runtime) evalscript(state State, stack *stack.Stack, caller string, ssc
 
 		switch opcode {
 		case OP_TRANSFER:
-			// TODO: Implement logic
+			amount := stack[len(stack)-1].(int)
+			stack = stack[:len(stack)-1]
+			to := stack[len(stack)-1].(string)
+			stack = stack[:len(stack)-1]
+			log.Println("transfer ", to, amount, caller)
+			if amount > state.bal[caller] {
+				log.Println("out of funds")
+			} else {
+				log.Println("enough funds. transfer")
+				x := state.bal[caller]
+				state.bal[caller] = x - amount
+				y := state.bal[to]
+				state.bal[to] = y + amount
+			}
+
 		case OP_NMREG:
-			// TODO: Implement logic
+			// Ensure there's at least one value on the stack.
+			if len(stack) < 1 {
+				panic(errors.New("Not enough items on stack for OP_NMREG"))
+			}
+
+			// Pop the name from the stack.
+			name := stack[len(stack)-1].(string) // assuming the name is a string; adjust type accordingly
+			stack = stack[:len(stack)-1]
+
+			// Register the name against the caller.
+			if _, exists := state.names[name]; exists {
+				panic(errors.New("Name already registered"))
+			}
+
+			// Optionally, you can check registration costs or any other business logic here
+
+			state.names[name] = caller
+
+			log.Printf("register %s %s", name, caller)
+
 		case OP_NMLOAD:
-			// TODO: Implement logic
+			// Ensure there's at least one value on the stack.
+			if len(stack) < 1 {
+				panic(errors.New("Not enough items on stack for OP_NMLOAD"))
+			}
+
+			// Pop the name from the stack.
+			name := stack[len(stack)-1].(string) // assuming the name is a string; adjust type accordingly
+			stack = stack[:len(stack)-1]
+
+			// Load the owner of the name from the state.
+			owner, exists := state.names[name]
+			if !exists {
+				// If the name isn't registered, push a nil or an empty string.
+				// Adjust this behavior based on your application's requirements.
+				stack = append(stack, "")
+			} else {
+				stack = append(stack, owner)
+			}
+
+			log.Printf("load %s -> %s", name, owner)
+
 		case OP_SHA256:
 			// TODO: Implement logic
 		case OP_PUSH:
-			// TODO: Implement logic
+			if ptr >= len(sscript) {
+				panic(errors.New("OP_PUSH without a subsequent value"))
+			}
+
+			value := sscript[ptr]
+			stack = append(stack, value)
+			ptr++ // Increment the pointer to move past the value.
+
 		case OP_EQUALVERIFY:
-			// TODO: Implement logic
+			if len(stack) < 2 {
+				panic(errors.New("Not enough items on stack for OP_EQUALVERIFY"))
+			}
+
+			// Pop the top two values from the stack.
+			value1 := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			value2 := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+
+			// Check if the two values are equal.
+			if value1 != value2 {
+				panic(errors.New("OP_EQUALVERIFY failed: top two stack values are not equal"))
+			}
+
+			// Push 'true' onto the stack to indicate successful verification.
+			stack = append(stack, true)
+
 		case OP_CHECKSIGCALL:
-			// TODO: Implement logic
+			// Ensure there are at least two values on the stack.
+			if len(stack) < 2 {
+				panic(errors.New("Not enough items on stack for OP_CHECKSIGCALL"))
+			}
+
+			// Pop the message hash and DER encoded signature from the stack.
+			derSign := stack[len(stack)-1].(string) // assuming the values are strings; adjust types accordingly
+			stack = stack[:len(stack)-1]
+			msgHash := stack[len(stack)-1].(string)
+			stack = stack[:len(stack)-1]
+
+			publicKey := caller
+
+			// Perform signature verification (assuming a function like checkSig exists in Go).
+			isValid, err := checkSig(publicKey, msgHash, derSign)
+			if err != nil {
+				// If there's an error in signature verification, push false onto the stack.
+				stack = append(stack, false)
+			} else {
+				// Push the result of signature verification onto the stack.
+				stack = append(stack, isValid)
+			}
+
 		case OP_KHASH:
-			// TODO: Implement logic
+			// Ensure there's at least one value on the stack.
+			if len(stack) < 1 {
+				panic(errors.New("Not enough items on stack for OP_KHASH"))
+			}
+
+			// Pop the top value from the stack for hashing.
+			value := stack[len(stack)-1].(string) // assuming the value is a string; adjust type accordingly
+			stack = stack[:len(stack)-1]
+
+			// Compute the Keccak hash (assuming you have a function named keccak for this).
+			hash := keccak(value)
+
+			// Push the hash onto the stack.
+			stack = append(stack, hash)
 		default:
 			panic("Unrecognized opcode: " + string(opcode))
 		}
